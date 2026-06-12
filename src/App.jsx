8fc6b788e3904b19e3a594c4d664db4ca4075473
Rowland Chazima <apungurowland90@gmail.com>
2026-06-12 23:50:01 +0300

    FIXING ERROR TRIAL : three

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import Cards from "../components/Cards";
-import Footer from "../components/Footer";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Cards from "./components/Cards";
+import Footer from "./components/footer";
 
 export default function LandingPage() {
   return (
